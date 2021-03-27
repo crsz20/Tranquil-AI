6b6dbeff09d1e9e6a9a07b2c96f6ef782130cfbd
crsz20 <cris14.alex@gmail.com>
2021-03-27 15:26:11 -0500

    OpenAI API test

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 import os
+import openai
 from dotenv import load_dotenv
 #https://www.twilio.com/blog/environment-variables-python#:~:text=Below%20you%20can%20see%20how%20to%20import%20a,variable%20definitions%20in%20it%20to%20the%20os.environ%20dictionary.
 
 load_dotenv()
-print(os.environ['API_KEY'])
+openai.api_key = os.environ['API_KEY']
+response = openai.Completion.create(engine="davinci", prompt="This is a test", max_tokens=5)
+
+#openai.Engine.list()
+print(response.get("model"))
